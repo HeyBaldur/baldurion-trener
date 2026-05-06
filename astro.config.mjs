712cfdb91763fcd3b9754c32e981bc2610bf8f1c
Rudy Jaubert <fillocj.rc@outlook.com>
2026-05-06 10:18:28 +0200

    Update astro.config.mjs and add GitHub Actions workflow for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,29 @@
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: 'https://baldurion.com',
-  output: 'static',
+  site: "https://heybaldur.github.io",
+  base: '/baldurion-trener',
+  output: "static",
   vite: {
     plugins: [tailwindcss()],
   },
   integrations: [
     sitemap({
       i18n: {
-        defaultLocale: 'en',
+        defaultLocale: "en",
         locales: {
-          es: 'es-ES',
-          en: 'en-US',
-          pl: 'pl-PL',
+          es: "es-ES",
+          en: "en-US",
+          pl: "pl-PL",
         },
       },
     }),
   ],
   i18n: {
-    defaultLocale: 'en',
-    locales: ['es', 'en', 'pl'],
+    defaultLocale: "en",
+    locales: ["es", "en", "pl"],
     routing: {
       prefixDefaultLocale: false,
     },
